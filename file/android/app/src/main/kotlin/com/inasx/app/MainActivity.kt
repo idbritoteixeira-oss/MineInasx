2f9b74cb157d53d84cc29968e9c67678b9c8977d
Gael <52140346-idbritoteixeira@users.noreply.replit.com>
2026-02-09 05:04:04 +0000

    update: configurando criptografia enx e rotas

--- file/android/app/src/main/kotlin/com/inasx/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.inasx.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- file/replit.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.zulu17
-    pkgs.flutter
-    pkgs.dart
-  ];
-}
